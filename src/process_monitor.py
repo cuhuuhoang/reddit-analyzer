4ca0c4c99190a61a402c9105117f9c833879a388
hoangch <hoangch@coccoc.com>
2023-07-04 19:22:10 +0700

    inc check time

--- src/process_monitor.py
+++ src/process_monitor.py
@@ -129,7 +129,7 @@ class ProcessMonitor:
 
                 if not max_analyzed_by_created_hours \
                         or datetime.fromtimestamp(max_analyzed_by_created_hours['timestamp']) < \
-                        datetime.now() - timedelta(hours=2):
+                        datetime.now() - timedelta(hours=3):
                     # Notify if analyzed_by_created_hours is not updated for a long time
                     self.notify_error("analyzed_by_created_hours is not updated for a long time.")
 
