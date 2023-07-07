60ebf44b55e6d1dd9dd1a4b360b931a0654c22bb
hoangch <hoangch@coccoc.com>
2023-07-07 21:30:42 +0700

    inc check time

--- src/submissions_main.py
+++ src/submissions_main.py
@@ -13,15 +13,16 @@ if __name__ == '__main__':
 
     while True:
         logging.info(f"spark_analyzer")
-        spark_analyzer = SparkAnalyzer()
-        spark_analyzer.analyze_by_hours()
-        spark_analyzer.analyze_by_days()
-        spark_analyzer.stop()
 
         for subreddit in Config.get().subreddit_list():
             logging.info(f"Fetching new submissions for subreddit: '{subreddit}'")
             fetch_new_submissions(subreddit, sentiment_analyzer, 1000)
             time.sleep(10)
+
+        spark_analyzer = SparkAnalyzer()
+        spark_analyzer.analyze_by_hours()
+        spark_analyzer.analyze_by_days()
+        spark_analyzer.stop()
 
         monitor = ProcessMonitor()
         monitor.check_and_notify()
